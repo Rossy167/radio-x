9516041528e6f904c3674bb8f863b7858c70d55d
Rossy167 <37879439+Rossy167@users.noreply.github.com>
2019-12-16 20:10:18 +0000

    getting songs from the website
    can now get a list of tracks and artists from the site

--- getsongs.py
+++ getsongs.py
@@ -5,6 +5,25 @@ from bs4 import BeautifulSoup
 website = "https://www.radiox.co.uk/radio/last-played-songs/"
 page = requests.get(website)
 soup = BeautifulSoup(page.content, "html.parser")
+tracks = []
+artists = []
+
+for a in soup.find_all("div", id=lambda x: x and x.startswith('song_promo_')):
+    try:
+        track = a.find('span', attrs={'class': 'track'})
+        artist = a.find('span', attrs={'class': 'artist'})
+        track = track.contents[0].strip()
+        artist = artist.contents[0].strip()
+        if artist == '':
+            artist = a.find('a', attrs={'class': 'first'})
+            artist = artist.contents[0].strip()
+        tracks.append(track)
+        artists.append(artist)
+    except:
+        isTrue = True
+
+print(tracks)
+print(artists)
 
 # so how the fuck do i read this...
 # i guess slice before the point where the list starts, then slice when it ends
