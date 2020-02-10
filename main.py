3ddd4e0455dff77015712506ef6d472f02d05a26
Rossy <louisross980@gmail.com>
2020-02-10 12:19:27 +0000

    big cleanup
    added files to ignore
    read in .csv now rather than .txt
    got rid of obsolete getsongs.py
    
    now ready for event scripting, will make a fork for a proper webapp version

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import csv
 import the_html_bit
 import the_spotify_section
 
-# temporary measure for authentication, putting tokens in .txt
-f = open("details.txt", "r")
-cid = f.readline().strip()
-secret = f.readline().strip()
-username = f.readline().strip()
-f.close()
+# QD solution for details, probably store as environment variables later
+with open('details.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for line in readCSV:
+        authentication = {
+            "cid": line[0].strip(),
+            "secret": line[1].strip(),
+            "username": line[2].strip()
+        }
 
 # songs are listed at this url
 url = "https://www.radiox.co.uk/radio/last-played-songs/"
@@ -16,7 +20,8 @@ html = the_html_bit.get_html(url)
 songs = the_html_bit.read_songs(html)
 
 # authenticate with spotify, lookup the songs then add them to a matching playlist (if doesn't exist then create one)
-session = the_spotify_section.simple_authenticate(cid, secret, username)
+session = the_spotify_section.simple_authenticate(authentication["cid"],
+                                                  authentication["secret"], authentication["username"])
 tracks = the_spotify_section.find_tracks_in_spotify(songs, session[0])
-playlist = the_spotify_section.get_playlist(session[0], username)
-the_spotify_section.add_to_playlist(tracks, session[0], username, playlist)
+playlist = the_spotify_section.get_playlist(session[0], authentication["username"])
+the_spotify_section.add_to_playlist(tracks, session[0], authentication["username"], playlist)
